4c64fbb73b91c5a3a4e81b2dea57e90bcf44c652
lvhao <lvhao@classba.cn>
2018-08-24 14:57:06 +0800

    18-08-24 complete some xadmin function

---
+++ LvShop/apps/users/adminx.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/23 17:08
+# @Author  : HaoLv
+
+import xadmin
+from xadmin import views
+from .models import VerifyCode
+
+
+class BaseSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings(object):
+    site_title = "慕学生鲜后台"
+    site_footer = "mxshop"
+    # menu_style = "accordion"
+
+
+class VerifyCodeAdmin(object):
+    list_display = ['code', 'mobile', "add_time"]
+
+
+xadmin.site.register(VerifyCode, VerifyCodeAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
---
+++ LvShop/extra_apps/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/8/22 16:23
+# @Author  : HaoLv
+
